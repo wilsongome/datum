c78166d1de42c76c91b854bc90580b0031448d5e
Wilson <wilsongome@gmail.com>
2023-07-14 13:33:43 -0300

    Implementando rota de consulta na controller

--- app/Http/Controllers/HashHandlerController.php
+++ app/Http/Controllers/HashHandlerController.php
@@ -2,18 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Domain\HashHandler;
+use App\Models\RequestResult;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HashHandlerController extends Controller
 {
     public function generate(Request $request)
     {
-        //Aqui farei validações
+        $request->merge(['str' => $request->route('str')]);
+        $validator = Validator::make(
+            $request->all(), 
+            [
+            'str' => 'required|max:255'
+        ]);
+        if ($validator->fails()) {
+            return response([], 400);
+        }
 
-        $hashHandler = new HashHandler($request->str);
-        $hashResult = $hashHandler->execute();
+        try{
+            $hashHandler = new HashHandler($request->str);
+            $hashResult = $hashHandler->execute();
+            return response()->json($hashResult, 200);
+        }catch(Exception $e){
+            Log::error($e);
+            return response()->json([], 500);
+        }
         
-        return response()->json($hashResult, 200);
     }
+
+    public function results(Request $request)
+    {
+        $request->merge(['page' => $request->route('page')]);
+        $validator = Validator::make($request->all(), [
+            'page' => 'required|numeric|gt:0',
+            'tries' => 'numeric|gt:0'
+        ]);
+
+        if ($validator->fails()) {
+            return response([], 400);
+        }
+       
+        $columns = ['batch', 'order_number', 'str_in', 'key_found'];
+        $registersPerPage = 20;
+
+        try{
+            if($request->tries){
+                $result = RequestResult::where('tries', '<', $request->tries)->paginate($registersPerPage, $columns, 'page', $request->page);
+            }else{
+                $result = RequestResult::paginate($registersPerPage, $columns, 'page', $request->page);
+            }
+            return response()->json($result->items(), 200);
+        }catch(Exception $e){
+            Log::error($e);
+            return response()->json([], 500);
+        }
+    }
+
 }
